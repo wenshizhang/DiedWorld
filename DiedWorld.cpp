1499c1dfa4486267d1b9a91cca545338f53c0ff8
Shiwen Zhang <shi.safari@gmail.com>
2017-06-03 13:56:09 +0800

    Matrix success 3*3
    Signed-off-by: Shiwen Zhang <shi.safari@gmail.com>

--- DiedWorld.cpp
+++ DiedWorld.cpp
@@ -6,9 +6,13 @@ int main()
 	vector<pair<int,int>> vect;
     vect.push_back(pair<int,int>(1,0)); 	
     vect.push_back(pair<int,int>(1,2)); 	
-    vect.push_back(pair<int,int>(1,1)); 	
+    vect.push_back(pair<int,int>(2,0)); 	
+//    vect.push_back(pair<int,int>(1,2)); 	
+ //   vect.push_back(pair<int,int>(2,1)); 	
 	auto ret = transVector(vect,3,3);
 	Generation world(ret);
+
+	world.show();
 	while(world.getGnrtNum()<10000){
 		world.nextGeneration();
 		world.show();
