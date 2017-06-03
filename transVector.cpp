549c88242c3f8488858ed45ed916ba6f955fa100
chenlin2844 <907169968@qq.com>
2017-06-03 13:03:39 +0800

    add main func

---
+++ transVector.cpp
@@ -0,0 +1,10 @@
+#include "transVector.h"
+using namespace std;
+vector<vector<int> > transVector(vector<pair<int,int>> vect_pair,int row, int col)
+{
+	vector<vector<int>> retVect(col,vector<int>(row));
+	for(auto iter = vect_pair.begin();iter!=vect_pair.end();iter++){
+		retVect[iter->first][iter->second] = 1;
+	}
+	return retVect;
+}
