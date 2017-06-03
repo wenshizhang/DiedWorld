8ad513dc312ec54ea59d952209b63cd54e56066e
chenlin2844 <907169968@qq.com>
2017-06-03 14:55:52 +0800

    add test file

--- testWorld.cpp
+++ testWorld.cpp
@@ -1,7 +1,16 @@
 #include "Generation.h"
 #include <gtest/gtest.h>
-
+#include "transVector.h"
 TEST(newGeneration,test3){
 	vector<vector<int > > vect(3,vector<int>(3));
-	Generation nowGen();
+    vect.push_back(pair<int,int>(1,0)); 	
+    vect.push_back(pair<int,int>(1,1)); 	
+    vect.push_back(pair<int,int>(1,2)); 	
+	vector<vector<int > > vect_EXPECT(3,vector<int>(3));
+    vect_EXPECT.push_back(pair<int,int>(0,1)); 	
+    vect_EXPECT.push_back(pair<int,int>(1,1)); 	
+    vect_EXPECT.push_back(pair<int,int>(2,1)); 	
+	Generation nowGen(vect);
+	nowGen.nextGeneration();
+	EXPECT_EQ(nowGen.getCellWorld(),vect_EXPECT);
 }
