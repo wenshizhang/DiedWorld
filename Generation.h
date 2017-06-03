fc843074488ec53962c09620882e78ecf5192fc8
Shiwen Zhang <shi.safari@gmail.com>
2017-06-03 12:10:05 +0800

    Midify class Generation
    Signed-off-by: Shiwen Zhang <shi.safari@gmail.com>

--- Generation.h
+++ Generation.h
@@ -8,13 +8,28 @@ class Generation
 	private:
 		vector<vector<int> > cellWorld;
 		int GnrtNum;
+		int Row;
+		int Col;
 	public:
-		Generation():GnrtNum(0){};
-		Generation(vector<vector<int> > &cw):cellWorld(cw),GnrtNum(0){};
+		typedef vector<vector<int>> matrix_int;
+		Generation():GnrtNum(0),Row(0),Col(0){};
+		Generation(vector<vector<int> > &cw):cellWorld(cw),GnrtNum(0){
+			if(!cw.empty()&&!cw[0].empty()){
+				Col = cw.size();
+				Row = cw[0].size();
+			}
+		};
 		
-		vector<vector<int> > nextGeneration();
-		vector<vector<int> > getCellWorld(){return cellWorld;}
+
+		void nextGeneration();
+		matrix_int getDiedOrLive();
+		int calcCellState(int x,int y);
+		void update(matrix_int);
+
 		int getGnrtNum(){return GnrtNum;}
+		int getRow(){return Row;}
+		int getCol(){return Col;}
 		void resetCellWorld(vector<vector<int> > &cw){cellWorld = cw; GnrtNum = 0;}
+		vector<vector<int> > getCellWorld(){return cellWorld;}
 		void show();
 }
